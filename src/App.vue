1218dfc9d8fb188341c9127373fca8543c54d7f7
chnyang <chn.yang@qq.com>
2023-03-28 19:46:31 +0800

    vue demo

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HiComponent name="vue" />
+  <router-view/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import HiComponent from './components/Hi.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    HiComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Hi.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1>Hi {{ name }}</h1>
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HiComponent",
+  props: {
+    name: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
